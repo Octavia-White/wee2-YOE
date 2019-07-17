3f107c835edd99896dbc34bcd5ecf094bcc3b5eb
Octavia-White <52667326+Octavia-White@users.noreply.github.com>
2019-07-17 13:58:06 -0400

    Add files via upload

---
+++ day8/FizzBuzz.java
@@ -0,0 +1,29 @@
+package day8;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		
+		for (int x= 1; x<=100;x++) {
+		 
+		 
+		 System.out.println(x);
+		
+		 if (x%3==0 && (x%5==0)){
+			
+			 System.out.println("fizz"); 
+			 }if  (x%5==0) {
+				  System.out.println("buzz");
+		 		 
+			 }else if ((x%3==0) && (x%5==0)) {
+				 System.out.println("fissbuzz");
+			 }
+			 }
+			 }
+}
+	
+			
+		
+
