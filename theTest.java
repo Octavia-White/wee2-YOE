7c79211817653e3627a07b1e72a105ff2f81d4a0
Octavia-White <52667326+Octavia-White@users.noreply.github.com>
2019-07-23 13:45:10 -0400

    Update theTest.java
    Finished version.

--- theTest.java
+++ theTest.java
@@ -2,59 +2,83 @@ import java.util.Scanner;
 
 public class theTest {
 
+	import java.util.Scanner;
+
+public class theTest {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner scam = new Scanner (System.in);
-
+		Scanner scam = new Scanner(System.in);
+		
 		String name = "";
-		System.out.println(" What is your name ");		
-		
-		
+		System.out.println("Enter your name please.");
 		name = scam.next();
-		System.out.println("Hello " + name +" Please answer the following questions"  );
+	    String sport = "";
+	    String food = "";
+	
+		System.out.println("Hello " + name + " please answer our short survey. ");
 		
-		System.out.println("what is your favorite sport" ); 
-		scam.next();
+		System.out.println("What's your favorite sport?");
+		sport = scam.next();
 		
-		System.out.println("what is your favorite food" );
-		scam.next(); 
+		System.out.println("What's your favorite food?");
+		food = scam.next();
+		
+		
+		System.out.println("Wow, " + sport + " is an amazing sport that I'm pretty sure you're very good at. I see " + food + " is your favorite food. Good choice, I love" + food + "myself." );
 		
 		int color = 15;
-		int red = 20; 
-		int pink =4; 
+		int red = 20;
+		int pink = 4;
 		
-		System.out.println("need a calculto r" + name );
+	
 		
+		System.out.println("Have anything adding, subtracting, multiplying, or dividing to do?" + name + " We have a calculator on deck for you. Would you like to try it out? Answer with yes or no please." );
+		
+		String answer = "";
+		answer = scam.next();
+		
+		if (answer.equalsIgnoreCase("yes")){
+		
+		System.out.println("Enter a number");
+		color = scam.nextInt();
+		
+		System.out.println("Enter another number");
+		red = scam.nextInt();
+		
+		
+		
+		System.out.println("This is the sum");
+		 pink = red + color ;
+		 System.out.println(pink);
+		 
+		 System.out.println("This is the difference");
+		 pink = red - color;
+		 System.out.println(pink);
+		 
+		 System.out.println("This is the product");
+		 pink = color * red ;
+		 System.out.println(pink);
+		 
+		 System.out.println("This is the quotient");
+		 pink = red / color ;
+		 System.out.println(pink); 
+		 
+		 System.out.println("Thank you for taking our survey and trying our calculator.");
+		 
+		} else {
 				
-		System.out.println("enter a number");
-		color =scam.nextInt();
-		
-		System.out.println("enter another number");
-		red =scam.nextInt(); 
-		
-		System.out.println("enter the last number");
-		pink = scam.nextInt(); 
-		
-		System.out.println("this is the sum");
-		color = pink + red; 	
-		System.out.print(color);
-		
-		System.out.println(" thios is the difference");
-		color = pink- red; 
-		System.out.print(color);
-		
-		System.out.println("this is the product");
-		color = pink * red;
-		System.out.print(color);
-		
-		System.out.println("this is the quotient ");
-		color = pink/red ; 
-		System.out.print(color);
-		
-		
-		
-		
+			System.out.println("Ok thank you for your participation.");
+			
+			
+		} 
 	}
 
-}
+	}
+	 
+		 
+		
+		
+				
+
